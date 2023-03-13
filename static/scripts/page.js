b534ada75d88f9ba304d9a8b3f8882bf19b8baad
Christopher Fuchs <christopher@christopherfuchs.de>
2023-03-13 16:56:03 +0100

    fixed collapsed menu  layout changes for smaller screens

---
+++ static/scripts/page.js
@@ -0,0 +1,75 @@
+$(function() {
+    initListeners();
+});
+
+
+function fadeAndLoad(htmlFile)  {
+    let node = $("#content");
+
+    if( node.hasClass("custom-notVisible")) {
+        node.removeClass("custom-notVisible");
+    }
+
+    if( node.hasClass("custom-fadeIn")) {
+        node.removeClass("custom-fadeIn");
+        node.addClass("custom-fadeOut");
+
+        setTimeout(function() {
+            node.removeClass("custom-fadeOut");
+            fadeAndLoad(htmlFile);
+        }, 1000);
+        return;
+    }
+    
+    loadContent(node, htmlFile);
+    node.addClass("custom-fadeIn");        
+}
+
+function loadContent(node, htmlFile) {
+    let language = $('meta[name=language]').attr('content');
+
+    
+    if( language != undefined) {
+        htmlFile +=  "?language=" + language;
+    }
+    
+    node.load(htmlFile);        
+}
+
+function initListeners() {    
+    $("#hrefGermanLanguage").on("click",function() {
+        document.location.href = "?language=de";
+    });
+
+    $("#hrefEnglishLanguage").on("click",function() {
+        document.location.href = "?language=en";
+    });
+
+
+    $("#hrefAboutMe").on("click",function() {
+        fadeAndLoad( "aboutMe.html");
+    });
+
+    $("#hrefProjects").on("click",function()  {
+        fadeAndLoad( "projects.html");
+    });
+
+    $("#hrefSkills").on("click",function()  {
+        fadeAndLoad( "skills.html");
+    });
+
+    $("#hrefHobbies").on("click",function()  {
+        fadeAndLoad( "hobbies.html");
+    });
+
+    $("#hrefContact").on("click",function(){
+        fadeAndLoad( "contact.html");
+    });
+
+    $("#hrefLegalInfo").on("click",function(){
+        fadeAndLoad( "legalInfo.html");
+    });
+    $("#hrefAboutPage").on("click",function(){
+        fadeAndLoad( "aboutPage.html");
+    });
+}
