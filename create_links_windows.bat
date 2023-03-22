7710c5c13a1e5e1ff7c7f3eedb83592cde06a38d
Christopher Fuchs <christopher@christopherfuchs.de>
2023-03-22 11:39:50 +0100

    added script to create links to other project

---
+++ create_links_windows.bat
@@ -0,0 +1,3 @@
+mklink static\images\3424-B23-017(c)SoulPicture.jpg ..\rust-website-private-config\static\images\3424-B23-017(c)SoulPicture.jpg /h
+mklink static\images\wrapper_header_bg.jpg ..\rust-website-private-config\static\images\wrapper_header_bg.jpg /h
+mklink static\images\logo.jpg ..\rust-website-private-config\static\images\logo.jpg /h
